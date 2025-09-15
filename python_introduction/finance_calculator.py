185a9a1fd3b060c02d0e448d5cf8b53cd547ab14
AbdElhameed <medo91057@gmail.com>
2025-09-15 15:48:49 +0300

    refactor finance calculator to use float for income and expenses inputs

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,10 @@
-monthly_income = input("Enter your monthly income: ")
-monthly_expenses = input("Enter your monthly expenses: ")
-monthly_savings = int(monthly_income) - int(monthly_expenses)
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+print(f"Your monthly savings are ${monthly_savings:.0f}.")
+
 interest_rate = 0.05
+
 annual_savings = int(monthly_savings * 12 + (monthly_savings * 12 * interest_rate))
-print("Your monthly savings are:", monthly_savings)
-print("Projected savings after one year, with interest, is:", annual_savings)
+print(f"Projected savings after one year, with interest, is: ${annual_savings:.0f}.")
