379e64bb0fd7f6978050391fa07561173cb26760
abdelhamiedDev <medo91057@gmail.com>
2025-11-10 23:15:57 +0200

    Add SimpleCalculator class and corresponding unit tests for basic arithmetic operations

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,26 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calculator.add(2, 3), 5)
+        self.assertEqual(self.calculator.add(-1, 1), 0)
+        self.assertEqual(self.calculator.add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtract(5, 3), 2)
+        self.assertEqual(self.calculator.subtract(0, 0), 0)
+        self.assertEqual(self.calculator.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(4, 3), 12)
+        self.assertEqual(self.calculator.multiply(-1, 5), -5)
+        self.assertEqual(self.calculator.multiply(0, 100), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(10, 2), 5)
+        self.assertEqual(self.calculator.divide(-6, 3), -2)
+        self.assertIsNone(self.calculator.divide(5, 0))
